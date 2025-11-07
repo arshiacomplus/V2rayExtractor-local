a5257c4882913a5d910f84fad8a54e75ac54a1f7
Arshia <163334604+arshiacomplus@users.noreply.github.com>
2025-11-07 17:53:37 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     print("🚀 Starting V2Ray Scraper Web UI Server...")
     print("✅ Access the UI at: http://127.0.0.1:8000")
     uvicorn.run(
-        "main:app",
+        app,
         host="127.0.0.1",
         port=8000,
         log_level="info"
